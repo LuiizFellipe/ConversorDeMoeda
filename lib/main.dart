6f7f2b5478c3bdf7bc844ffec2a6f987bac87934
Luiizfellipe <luiizfellipe12@gmail.com>
2020-04-05 21:01:54 -0300

    Feita a implementação de arquivo JSON e adicionado AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main(){
+const request = "https://api.hgbrasil.com/finance";
+
+void main()async{
   runApp(MaterialApp(
-    home: Container(),
+    home: Home()
   ));
+}
+
+//Futuro acontece apenas no futuro não no momento
+Future<Map> getData() async{
+    http.Response response = await http.get(request);
+    return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Convesor de Moedas"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
