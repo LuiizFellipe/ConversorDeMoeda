1a6d1d883af4a665b07cad36e49af4208b8dccf8
Luiizfellipe <luiizfellipe12@gmail.com>
2020-04-05 21:30:57 -0300

    Construção do FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,14 @@ import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance";
 
-void main()async{
-  runApp(MaterialApp(
-    home: Home()
-  ));
+void main() async {
+  runApp(MaterialApp(home: Home()));
 }
 
 //Futuro acontece apenas no futuro não no momento
-Future<Map> getData() async{
-    http.Response response = await http.get(request);
-    return json.decode(response.body);
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -26,12 +24,38 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("Convesor de Moedas"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("Convesor de Moedas"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                      child: Text(
+                    "Carregando dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ));
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text(
+                      "Erro ao carregar os dados, verifique a sua conexão",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ));
+                  }
+                  else{
+                    return Container(color: Colors.green,);
+                  }
+              }
+            })
     );
   }
 }
